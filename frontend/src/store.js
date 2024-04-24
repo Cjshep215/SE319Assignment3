3c1baf2778d091146ae1ad23ecc533bad19bff6e
Luke Herbsleb <luke@herbsleb.com>
2024-04-23 19:43:19 -0500

    working on navbar

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -243,6 +243,39 @@ export function Store() {
   return (
     <div>
       <button onClick={setViewOne}>One</button>
+      {/* navbar */}
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+    <div class="container-fluid">
+      <a class="navbar-brand">Fake Store</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample03">
+        <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Add</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">View</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Update</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Delete</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Student Information</a>
+          </li>
+
+          
+          
+        </ul>
+      </div>
+    {/* can add a thing on the right here */}
+    </div>
+  </nav>
 
       {oneView === 0 && <View1 />}
       {oneView === 1 && <View2 />}
