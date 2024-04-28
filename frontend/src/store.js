f017266ff9535fe0105b75017e985fe4eb970431
Connor Shepherd <cjshep@iastate.edu>
2024-04-27 19:56:29 -0500

    more

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -129,38 +129,39 @@ export function Store() {
   }
 
   function View2() {
-    let favProduct = <div></div>;
-
-    if (showId != 0) {
-      let tmpId = showId;
-      fetch(`http://localhost:8081/${tmpId}`)
-        .then((response) => response.json())
-        .then((myFavProduct) => {
-          console.log(myFavProduct);
-          setMyFavProduct(myFavProduct);
-        });
-
-      favProduct = (
-        <div className="card">
-          <img
-            src={myFavProduct.image}
-            className="card-img-top"
-            alt={myFavProduct.title}
-            style={{ objectFit: "cover", height: "auto" }} //This fits cards to same size
-          />
-          <div className="card-body">
-            <h5 className="card-title">{myFavProduct.title}</h5>
-            <p className="card-text">{myFavProduct.description}</p>
-            <p className="card-price">${myFavProduct.price}</p>
-            <p className="card-rating">
-              Rating: {myFavProduct.rating.rate}/5 - (
-              {myFavProduct.rating.count} reviews)
-            </p>
-            <p className="card-category">Category: {myFavProduct.category}</p>
-            <p className="card-id">Id: {myFavProduct.id}</p>
+    function FavProduct(){
+      if (showId != 0) {
+        let tmpId = showId;
+        fetch(`http://localhost:8081/${tmpId}`)
+          .then((response) => response.json())
+          .then((myFavProduct) => {
+            console.log(myFavProduct);
+            setMyFavProduct(myFavProduct);
+          });
+  
+        return (
+          <div className="card">
+            <img
+              src={myFavProduct.image}
+              className="card-img-top"
+              alt={myFavProduct.title}
+              style={{ objectFit: "cover", height: "auto" }} //This fits cards to same size
+            />
+            <div className="card-body">
+              <h5 className="card-title">{myFavProduct.title}</h5>
+              <p className="card-text">{myFavProduct.description}</p>
+              <p className="card-price">${myFavProduct.price}</p>
+              <p className="card-rating">
+                Rating: {myFavProduct.rating.rate}/5 - (
+                {myFavProduct.rating.count} reviews)
+              </p>
+              <p className="card-category">Category: {myFavProduct.category}</p>
+              <p className="card-id">Id: {myFavProduct.id}</p>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
+      return<div></div>;
     }
 
     return (
@@ -203,7 +204,7 @@ export function Store() {
           <button type="submit">show one product by id</button>
         </form>
         <div id="col2">
-          <favProduct />
+          <FavProduct />
         </div>
         <br />
       </div>
